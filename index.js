02fac00121bcd52abc2818b8e69fb8dfe15c2d46
Mamunur Rahman <mamunur@mac-air.home>
2023-08-22 17:23:33 +0100

    console log netChangeSum

--- index.js
+++ index.js
@@ -108,11 +108,17 @@ for(let index = 0; index < finances.length; index++){
         change = finances[index][index2] - net;
         net = finances[index][index2];
         netArray.push(change);
-        console.log(`total: ${total}`);
-        console.log(`change: ${change}`);
-        console.log(`net: ${net}`);
-        console.log(`netArray ${netArray}`);
+        //console.log(`total: ${total}`);
+        //console.log(`change: ${change}`);
+        //console.log(`net: ${net}`);
+        //console.log(`netArray ${netArray}`);
 
       }
     }
 }
+
+for(let index = 0; index < netArray.length; index++){
+  netChangeSum += netArray[index];
+}
+
+console.log(netChangeSum)
